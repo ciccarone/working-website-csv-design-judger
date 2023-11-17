2dbd441e8ac301aa460c04377d0c2924c3fa6ddb
Tony Ciccarone <tonyvciccarone@gmail.com>
2023-11-17 13:50:06 -0500

    scroll through the url for full testing

--- screenshot.js
+++ screenshot.js
@@ -1,13 +1,30 @@
 const puppeteer = require('puppeteer');
 const path = require('path');
-const fsPromises = require('fs').promises;
-
 
 (async () => {
     const browser = await puppeteer.launch();
     const page = await browser.newPage();
-    await page.goto(process.argv[2], { waitUntil: 'networkidle0' });
+    await page.goto(process.argv[2], {waitUntil: 'networkidle0'});
+
+    // Scroll through the page
+    await page.evaluate(async () => {
+        await new Promise((resolve, reject) => {
+            var totalHeight = 0;
+            var distance = 100;
+            var timer = setInterval(() => {
+                var scrollHeight = document.body.scrollHeight;
+                window.scrollBy(0, distance);
+                totalHeight += distance;
+
+                if (totalHeight >= scrollHeight){
+                    clearInterval(timer);
+                    resolve();
+                }
+            }, 100);
+        });
+    });
+
     const screenshotPath = path.join(process.cwd(), './frames/site.png');
-    await page.screenshot({ path: screenshotPath, fullPage: true });
+    await page.screenshot({path: screenshotPath, fullPage: true});
     await browser.close();
 })();
